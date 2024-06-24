86114cb5a0bcef723ba6d369db87b3121e7aebd9
Rafael Alves <60042784+Rafael540@users.noreply.github.com>
2024-06-24 17:42:55 -0300

    Commit

--- santander-dev-week-2023/src/main/java/me/dio/Application.java
+++ santander-dev-week-2023/src/main/java/me/dio/Application.java
@@ -1,8 +1,17 @@
 package me.dio;
 
+<<<<<<< HEAD
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
+=======
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+>>>>>>> dd7d6698c1de76eb052ea99ab9d6623e38285d21
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +19,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> dd7d6698c1de76eb052ea99ab9d6623e38285d21
---
+++ santander-dev-week-2023/src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,11 @@
+package me.dio.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+@Entity(name = "tb_features")
+public class Feature  extends BaseItem{
+	
+	
+	
+}
